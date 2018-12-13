0558c7cb48a2b56762938decf7594cd6f0586b4f
Mathieu <mathieu@math98.fr>
2018-12-13 15:23:44 +0100

    Ajoute le Catalogue

---
+++ src/Modele/Catalogue.java
@@ -0,0 +1,121 @@
+package Modele;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Catalogue implements I_Catalogue {
+    private ArrayList<I_Produit> produits = new ArrayList<>();
+
+    public Catalogue() {
+
+    }
+
+    @Override
+    public boolean addProduit(I_Produit produit) {
+        if(this.produits.indexOf(produit) == -1) {
+            this.produits.add(produit);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addProduit(String nom, double prix, int qte) {
+        if (prix <= 0 || qte <= 0) {
+            return false;
+        }
+
+        I_Produit produit = new Produits(nom, prix, qte);
+        return addProduit(produit);
+    }
+
+    @Override
+    public int addProduits(List<I_Produit> l) {
+        int i = 0;
+        for (I_Produit produit : l) {
+            if(this.addProduit(produit)) {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    @Override
+    public boolean removeProduit(String nom) {
+        try {
+            I_Produit produit = findProduit(nom);
+            produits.remove(produit);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private I_Produit findProduit(String nom) {
+        for (I_Produit produit : produits) {
+            if (produit.getNom().equals(nom)) {
+                return produit;
+            }
+        }
+
+        throw new NoSuchElementException("Produit introuvable");
+    }
+
+    @Override
+    public boolean acheterStock(String nomProduit, int qteAchetee) {
+        try {
+            I_Produit produit = findProduit(nomProduit);
+            if (qteAchetee <= 0) {
+                return false;
+            }
+
+            produit.ajouter(qteAchetee);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean vendreStock(String nomProduit, int qteVendue) {
+        try {
+            I_Produit produit = findProduit(nomProduit);
+            if (qteVendue <= 0) {
+                return false;
+            }
+
+            if (produit.getQuantite() < qteVendue) {
+                throw new IllegalArgumentException("La quantité vendue ne peut être supérieure à la quantité stockée !");
+            }
+
+            produit.enlever(qteVendue);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String[] getNomProduits() {
+        ArrayList<String> noms = new ArrayList<>();
+        for (I_Produit produit : this.produits) {
+            noms.add(produit.getNom());
+        }
+        return (String[]) noms.toArray();
+    }
+
+    @Override
+    public double getMontantTotalTTC() {
+        double total = 0;
+        for(I_Produit produit : produits) {
+            total += produit.getPrixStockTTC();
+        }
+        return total;
+    }
+
+    @Override
+    public void clear() {
+        this.produits = new ArrayList<>();
+    }
+}
